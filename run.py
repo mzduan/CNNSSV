a367ae40ec3d5842fb67d9f658ee43178f0dea55
mzduan <278326988@qq.com>
2021-10-31 20:44:48 +0800

    update

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 if __name__ == '__main__':
     command = "python main.py --tumor '/Users/duan/Desktop/results/somaticSV/bam/CCS/0.7/somatic_bam_chr20/sim.srt.bam' --normal '/Users/duan/Desktop/results/somaticSV/bam/CCS/0.7/germline_bam_chr20/sim.srt.bam' --ref '/Users/duan/Downloads/data/ref/v38/chr20.fa'  --wkdir ~/Desktop/test -t 1"
-    #command="python main.py --tumor '/home/duan/Desktop/somaticSV/bam/NA19239_NA19240_mixed/0.7/mixed.chr20.bam' --normal '/home/duan/Desktop/somaticSV/bam/NA19239_NA19240_mixed/0.7/NA19239.chr20.bam' --ref '/home/duan/Data/ref/chr20.fa' --model '/home/duan/Desktop/somaticSV/cnn_10_24.model' --wkdir ~/Desktop/test --output ~/Desktop/output.bed -t 1"
+    # command="python main.py --tumor '/home/duan/Desktop/somaticSV/bam/NA19239_NA19240_mixed/0.7/mixed.chr20.bam' --normal '/home/duan/Desktop/somaticSV/bam/NA19239_NA19240_mixed/0.7/NA19239.chr20.bam' --ref '/home/duan/Data/ref/chr20.fa' --model '/home/duan/Desktop/somaticSV/cnn_10_24.model' --wkdir ~/Desktop/test --output ~/Desktop/output.bed -t 1"
     os.system(command)
 
 
