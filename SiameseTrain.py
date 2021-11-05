76ed07fa00b7c675f23e3ddd32b02d27913c8932
mzduan <278326988@qq.com>
2021-11-05 11:10:36 +0800

    update

--- SiameseTrain.py
+++ SiameseTrain.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     cuda_gpu = torch.cuda.is_available()
     # cuda_gpu=False
 
-    writer = SummaryWriter('/home/duan/Desktop/trainlog_v1')
+    writer = SummaryWriter('/home/mzduan/trainlog_v1')
     siamese= Siamese_v1.Siamese_V1()
     if cuda_gpu:
         siamese=siamese.cuda()
