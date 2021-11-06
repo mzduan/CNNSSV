2c5bc461cc11ea73d0932e4590a572f3b4bfaf3e
mzduan <278326988@qq.com>
2021-11-07 00:53:12 +0800

    update

--- SiameseTestNetworkDataset_v1.py
+++ SiameseTestNetworkDataset_v1.py
@@ -16,47 +16,45 @@ class SiameseTestNetworkDataset(data.Dataset):
             splits = f.split('_')
             if len(splits) == 4:
                 absolute_path = feat_path + '/' + f
+                n_flag = False
+                t_flag = False
+                nv_flag = False
+                tv_flag = False
+
+                n_feature = np.zeros((3, 50, 500))
+                t_feature = np.zeros((3, 50, 500))
+                n_feat = None
+                t_feat = None
                 for image in os.listdir(absolute_path):
-                    if f[0] != '.':
-                        n_flag = False
-                        t_flag = False
-                        nv_flag = False
-                        tv_flag = False
-
-                        n_feature = np.zeros((3, 50, 500))
-                        t_feature = np.zeros((3, 50, 500))
-                        n_feat = None
-                        t_feat = None
-
-                        if image == 'normal.npy':
-                            normal=np.load(absolute_path+'/'+image)
-                            normal = np.array(normal)
-                            n_feature[0] = normal[:, :, 0]
-                            n_feature[1] = normal[:, :, 1]
-                            t_feature[2] = normal[:, :, 2]
-                            n_flag = True
-                        elif image == 'tumor.npy':
-                            tumor=np.load(absolute_path+'/'+image)
-                            tumor = np.array(tumor)
-                            t_feature[0] = tumor[:, :, 0]
-                            t_feature[1] = tumor[:, :, 1]
-                            t_feature[2] = tumor[:, :, 2]
-                            t_flag = True
-                        elif image == 'normal_sup_feature.npy':
-                            n_feat = np.load(absolute_path + '/' + image)
-                            nv_flag = True
-                        elif image == 'tumor_sup_feature.npy':
-                            t_feat = np.load(absolute_path + '/' + image)
-                            tv_flag = True
-                        if n_flag==True and t_flag==True and nv_flag==True and tv_flag==True:
-                            self.normal_features.append(n_feature)
-                            self.tumor_features.append(t_feature)
-                            self.n_vector.append(n_feat)
-                            self.t_vector.append(t_feat)
-                            self.file_names.append(f)
+                    if image == 'normal.npy':
+                        normal=np.load(absolute_path+'/'+image)
+                        normal = np.array(normal)
+                        n_feature[0] = normal[:, :, 0]
+                        n_feature[1] = normal[:, :, 1]
+                        t_feature[2] = normal[:, :, 2]
+                        n_flag = True
+                    elif image == 'tumor.npy':
+                        tumor=np.load(absolute_path+'/'+image)
+                        tumor = np.array(tumor)
+                        t_feature[0] = tumor[:, :, 0]
+                        t_feature[1] = tumor[:, :, 1]
+                        t_feature[2] = tumor[:, :, 2]
+                        t_flag = True
+                    elif image == 'normal_sup_feature.npy':
+                        n_feat = np.load(absolute_path + '/' + image)
+                        nv_flag = True
+                    elif image == 'tumor_sup_feature.npy':
+                        t_feat = np.load(absolute_path + '/' + image)
+                        tv_flag = True
+                    if n_flag==True and t_flag==True and nv_flag==True and tv_flag==True:
+                        self.normal_features.append(n_feature)
+                        self.tumor_features.append(t_feature)
+                        self.n_vector.append(n_feat)
+                        self.t_vector.append(t_feat)
+                        self.file_names.append(f)
 
     def __len__(self):
-        print(len(self.normal_features))
+
         return len(self.normal_features)
 
     def __getitem__(self, index):
