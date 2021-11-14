c2a763f9a7d1e072061f7f6893e29671e0081260
mzduan <278326988@qq.com>
2021-11-14 12:45:10 +0800

    update

--- get_kmer_count.py
+++ get_kmer_count.py
@@ -2,7 +2,7 @@
 #除了image的特征外，额外融入序列特征(hash sequence)，坐标信息，比对质量信息
 import pysam
 import numpy as np
-
+from breakpoints import get_breakpoints
 def get_reverse_comp(s):
     ret=""
     for i in range(len(s)-1,-1,-1):
@@ -85,54 +85,179 @@ def get_somatic_kmer(sv_type,somatic_support_reads,somatic_bam_file,normal_bam_f
     if sv_type=='INS' or sv_type=='INV':
         for k in sorted_tumor_sv_kmer:
 
-            somatic_kmer[k[0]]=[0,0]
+            somatic_kmer[k[0]]=[k[1],0]
             count=count+1
             if count==62:
                 break
 
     elif sv_type=='DEL' or sv_type=='DUP':
         for k in sorted_tumor_sv_kmer:
-            somatic_kmer[k[0]] = [0, 0]
+            somatic_kmer[k[0]] = [k[1], 0]
             count = count + 1
             if count == 31:
                 break
 
-    #计算somatic_kmers在normal样本中出现的次数
+    #计算somatic_kmers 是否跨过了normal样本中的变异
+    cdel, cins, cinv, cdup = get_breakpoints(normal_bam_file, 1, chro=chro, start=ref_bk1 - 100, end=ref_bk2 + 1000)
+    normal_sv_del_reads=dict()
+    normal_sv_ins_reads = dict()
+    normal_sv_inv_reads = dict()
+    normal_sv_dup_reads = dict()
+    for key in cdel.keys():
+        for sv in cdel[key]:
+            for i in range(len(sv[2])):
+                read_name=sv[2][i]
+                read_start=sv[3][i]
+                normal_sv_del_reads[read_name]=read_start
+    for key in cins.keys():
+        for sv in cins[key]:
+            for i in range(len(sv[2])):
+                read_name=sv[2][i]
+                read_start=sv[4][i]
+                read_end=sv[5][i]
+                normal_sv_ins_reads[read_name]=[read_start,read_end]
+    for key in cinv.keys():
+        for sv in cinv[key]:
+            for i in range(len(sv[2])):
+                read_name=sv[2][i]
+                read_start=sv[3][i]
+                read_end=sv[4][i]
+                normal_sv_inv_reads[read_name]=[read_start,read_end]
+
+    for key in cdup.keys():
+        for sv in cdup[key]:
+            for i in range(len(sv[2])):
+                read_name=sv[2][i]
+                read_start=sv[3][i]
+                normal_sv_dup_reads[read_name]=read_start
 
 
+    normal_name2seq=dict()
     normal_bam = pysam.AlignmentFile(normal_bam_file, 'r')
     normal_region_reads = normal_bam.fetch(contig=chro, start=ref_bk1 - 1000, end=ref_bk2 + 1000)
     for aln in normal_region_reads:
-        print(aln.query_name)
         if aln.is_unmapped or aln.mapping_quality < 20:
             continue
         else:
-            seq = aln.query_sequence
-            for i in range(0, len(seq) - kmer_len + 1):
-                kmer = seq[i:i + kmer_len]
-                rkmer = get_reverse_comp(kmer)
-                mkmer = rkmer if rkmer < kmer else kmer
-                if mkmer in somatic_kmer.keys():
-                    print(i,end=" ")
-                    somatic_kmer[mkmer][1]=somatic_kmer[mkmer][1]+1
-        print(" ")
+            if aln.query_name in normal_sv_del_reads.keys() or aln.query_name in normal_sv_ins_reads.keys() or aln.query_name in normal_sv_inv_reads.keys() or aln.query_name in normal_sv_dup_reads.keys():
+                if aln.query_name in normal_name2seq.keys():
+                    continue
+                if not aln.is_reverse:
+                    normal_name2seq[aln.query_name] = aln.query_sequence
+                else:
+                    # print(aln.query_sequence)
+                    normal_name2seq[aln.query_name] = get_reverse_comp(aln.query_sequence)
+    normal_sv_kmer=dict()
+    for k in normal_sv_del_reads.keys():
+        current_read_seq=normal_name2seq[k]
+        read_sv_bk=normal_sv_del_reads[k]
+        for j in range(read_sv_bk - kmer_len, read_sv_bk1):  # k
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+    for k in normal_sv_ins_reads.keys():
+        current_read_seq=normal_name2seq[k]
+        read_sv_bk1=normal_sv_ins_reads[k][0]
+        read_sv_bk2 = normal_sv_ins_reads[k][1]
+        for j in range(read_sv_bk1 - kmer_len + 1, read_sv_bk1 + 1):
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+        for j in range(read_sv_bk2 - kmer_len, read_sv_bk2):
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+
+    for k in normal_sv_inv_reads.keys():
+        current_read_seq=normal_name2seq[k]
+        read_sv_bk1=normal_sv_inv_reads[k][0]
+        read_sv_bk2 = normal_sv_inv_reads[k][1]
+        for j in range(read_sv_bk1 - kmer_len + 1, read_sv_bk1 + 1):
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+        for j in range(read_sv_bk2 - kmer_len, read_sv_bk2):
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+    for k in normal_sv_dup_reads.keys():
+        current_read_seq=normal_name2seq[k]
+        read_sv_bk=normal_sv_dup_reads[k]
+        for j in range(read_sv_bk - kmer_len, read_sv_bk):
+            kmer = current_read_seq[j:j + kmer_len]
+            rkmer = get_reverse_comp(kmer)
+            mkmer = rkmer if rkmer < kmer else kmer
+
+            if mkmer not in normal_sv_kmer.keys():
+                normal_sv_kmer[mkmer] = 1
+            else:
+                normal_sv_kmer[mkmer] += 1
+
     normal_bam.close()
 
 
-    tumor_bam = pysam.AlignmentFile(somatic_bam_file, 'r')
-    tumor_region_reads = tumor_bam.fetch(contig=chro, start=ref_bk1 - 1000, end=ref_bk2 + 1000)
-    for aln in tumor_region_reads:
-        if aln.is_unmapped or aln.mapping_quality < 20:
-            continue
-        else:
-            seq = aln.query_sequence
-            for i in range(0, len(seq) - kmer_len + 1):
-                kmer = seq[i:i + kmer_len]
-                rkmer = get_reverse_comp(kmer)
-                mkmer = rkmer if rkmer < kmer else kmer
-                if mkmer in somatic_kmer.keys():
-                    somatic_kmer[mkmer][0]=somatic_kmer[mkmer][0]+1
-    tumor_bam.close()
+    for k in somatic_kmer:
+        if k in normal_sv_kmer:
+            somatic_kmer[k][1]=normal_sv_kmer[k]
+
+
+
+
+
+    #计算somatic_kmers在normal样本中出现的次数
+
+    # normal_bam = pysam.AlignmentFile(normal_bam_file, 'r')
+    # normal_region_reads = normal_bam.fetch(contig=chro, start=ref_bk1 - 1000, end=ref_bk2 + 1000)
+    # for aln in normal_region_reads:
+    #     if aln.is_unmapped or aln.mapping_quality < 20:
+    #         continue
+    #     else:
+    #         seq = aln.query_sequence
+    #         for i in range(0, len(seq) - kmer_len + 1):
+    #             kmer = seq[i:i + kmer_len]
+    #             rkmer = get_reverse_comp(kmer)
+    #             mkmer = rkmer if rkmer < kmer else kmer
+    #             if mkmer in somatic_kmer.keys():
+    #                 somatic_kmer[mkmer][1]=somatic_kmer[mkmer][1]+1
+    # normal_bam.close()
+    #
+    #
+    # tumor_bam = pysam.AlignmentFile(somatic_bam_file, 'r')
+    # tumor_region_reads = tumor_bam.fetch(contig=chro, start=ref_bk1 - 1000, end=ref_bk2 + 1000)
+    # for aln in tumor_region_reads:
+    #     if aln.is_unmapped or aln.mapping_quality < 20:
+    #         continue
+    #     else:
+    #         seq = aln.query_sequence
+    #         for i in range(0, len(seq) - kmer_len + 1):
+    #             kmer = seq[i:i + kmer_len]
+    #             rkmer = get_reverse_comp(kmer)
+    #             mkmer = rkmer if rkmer < kmer else kmer
+    #             if mkmer in somatic_kmer.keys():
+    #                 somatic_kmer[mkmer][0]=somatic_kmer[mkmer][0]+1
+    # tumor_bam.close()
 
 
     # sorted(somatic_kmer.items(), key=lambda item: item[1][0], reverse=True)
@@ -167,6 +292,6 @@ def get_somatic_kmer(sv_type,somatic_support_reads,somatic_bam_file,normal_bam_f
             tumor_vector[i]=(tumor_vector[i]-min_count)/(max_count-min_count)
             normal_vector[i] = (normal_vector[i] - min_count) / (max_count - min_count)
 
-    # print(np.array(tumor_vector,dtype=np.float64))
-    # print(np.array(normal_vector,dtype=np.float64))
+    print(np.array(tumor_vector,dtype=np.float64))
+    print(np.array(normal_vector,dtype=np.float64))
     return np.array(tumor_vector,dtype=np.float64),np.array(normal_vector,dtype=np.float64)
