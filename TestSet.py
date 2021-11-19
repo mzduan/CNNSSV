e97a39a978374836084892e040c996aa92fa6204
mzduan <278326988@qq.com>
2021-11-19 20:54:38 +0800

    update

--- TestSet.py
+++ TestSet.py
@@ -14,13 +14,15 @@ class TestSet(data.Dataset):
                 absolute_path=feat_path+'/'+f
                 sub_feature = np.zeros((6, 50, 500))
                 for image in os.listdir(absolute_path):
-                    if image == 'normal.npy':
-                        normal = np.load(absolute_path + '/' + image)
+                    if image[0] == 'n':
+                        normal = Image.open(absolute_path + '/' + image)
+                        normal = np.array(normal)
                         sub_feature[3] = normal[:, :, 0]
                         sub_feature[4] = normal[:, :, 1]
                         sub_feature[5] = normal[:, :, 2]
-                    elif image[0] == 'tumor.npy':
-                        tumor = np.load(absolute_path + '/' + image)
+                    elif image[0] == 't':
+                        tumor = Image.open(absolute_path + '/' + image)
+                        tumor = np.array(tumor)
                         sub_feature[0] = tumor[:, :, 0]
                         sub_feature[1] = tumor[:, :, 1]
                         sub_feature[2] = tumor[:, :, 2]
