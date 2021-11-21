841ca8ce88c261bc36de0a142308608c1982ad73
mzduan <278326988@qq.com>
2021-11-21 19:21:31 +0800

    update

--- features.py
+++ features.py
@@ -215,20 +215,20 @@ def get_revised_reference(sv_type,chro,bk,ref_dict,somatic_bam_file,germline_bam
 
     # tumor_kmer_vector, normal_kmer_vector = get_somatic_kmer(sv_type, somatic_support_reads,somatic_bam_file,germline_bam_file, ref_dict, chro, bk)
     #
-    # type_counts,medium,mean_region_counts,mean_read_counts,mean_clus,max_clus=get_somatic_kmer(sv_type, somatic_support_reads,
-    #                                                      germline_bam_file, ref_dict, chro, bk)
+    type_counts,medium,mean_region_counts,mean_read_counts,mean_clus,max_clus=get_somatic_kmer(sv_type, somatic_support_reads,
+                                                         germline_bam_file, ref_dict, chro, bk)
     # print(type_counts,medium,mean_region_counts,mean_read_counts,mean_clus,max_clus)
     # somatic_rc=len(somatic_support_reads)+len(somatic_ref_reads)
     # germline_rc=len(germline_support_reads)+len(germline_support_reads)
     # r1=len(somatic_support_reads)/somatic_rc if somatic_rc>0 else 0
     # r2=len(germline_support_reads)/germline_rc if germline_rc>0 else 0
-    # sup_features = np.array([type_counts, medium, mean_region_counts, mean_read_counts, mean_clus, max_clus,len(somatic_support_reads),len(somatic_ref_reads),
-    #                          len(germline_support_reads),len(germline_ref_reads)])
+    sup_features = np.array([type_counts, medium, mean_region_counts, mean_read_counts, mean_clus, max_clus,len(somatic_support_reads),len(somatic_ref_reads),
+                             len(germline_support_reads),len(germline_ref_reads)])
     sv_str = output_dir + '/' + chro +'_'+sv_type + '_' + str(bk[0]) + '_' + str(bk[1])
     os.mkdir(sv_str)
     # np.save(sv_str + '/tumor_sup_feature',tumor_kmer_vector)
     # np.save(sv_str + '/normal_sup_feature',normal_kmer_vector)
-    # np.save(sv_str + '/sup_feat', sup_features)
+    np.save(sv_str + '/sup_feat', sup_features)
 
 
     #调整insertion位置，否则会导致reference 被多次添加gap
