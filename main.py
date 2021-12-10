03fa0ef5ddbf275bba356a57578bdb76e962886a
mzduan <278326988@qq.com>
2021-12-11 00:05:22 +0800

    update

--- main.py
+++ main.py
@@ -22,40 +22,40 @@ if __name__ == '__main__':
     parser.add_argument("-t", help="thead number", type=int, required=True)
     args = parser.parse_args()
 
-    if os.path.exists(args.wkdir):
-        os.system('rm -rf '+args.wkdir)
-        os.mkdir(args.wkdir)
-    else:
-        os.mkdir(args.wkdir)
-    print(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
-    print("Find Candidate SVs")
-
-    ref_dict = reference.initial_fa(args.ref)
-    cdel,cins,cinv,cdup=get_breakpoints(args.tumor,args.min_support_read,args.min_sv_len,args.max_sv_len,args.min_map_qual,ref_dict=ref_dict)
-    # DEL:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[len_list],mean_left_confu,mean_right_confu]
-    # INS:   [[pos,len,[read_name_list],insert_seq,[read_start_list],[read_end_list],[ref_start_list],[len_list],mean_left_confu,mean_right_confu]
-    # INV:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[ref_end_list],mean_left_confu,mean_right_confu]
-    # DUP:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[ref_end_list],mean_left_confu,mean_right_confu]
-
-    with open(args.wkdir+'/tumor.candidate.bed','w') as fout:
-        for key in cdel:
-            chro=key
-            for bk in cdel[chro]:
-                fout.write(chro+'\t'+str(bk[0])+'\t'+str(bk[0]+bk[1])+'\t'+'DEL\t'+str(bk[1])+'\n')
-        for key in cins:
-            chro=key
-            for bk in cins[chro]:
-                fout.write(chro+'\t'+str(bk[0])+'\t'+str(bk[0]+1)+'\t'+'INS\t'+str(bk[1])+'\n')
-        for key in cinv:
-            chro=key
-            for bk in cinv[chro]:
-                fout.write(chro + '\t' + str(bk[0]) + '\t' + str(bk[0] + bk[1]) + '\t' + 'INV\t'+str(bk[1])+'\n')
-        for key in cdup:
-            chro=key
-            for bk in cdup[chro]:
-                fout.write(chro + '\t' + str(bk[0]) + '\t' + str(bk[0] + bk[1]) + '\t' + 'DUP\t'+str(bk[1])+'\n')
-    print("Generate Features for Cancidate SVs")
-    run(cdel,cins,cinv,cdup,ref_dict,args.tumor,args.normal,args.wkdir,int(args.t))
+    # if os.path.exists(args.wkdir):
+    #     os.system('rm -rf '+args.wkdir)
+    #     os.mkdir(args.wkdir)
+    # else:
+    #     os.mkdir(args.wkdir)
+    # print(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
+    # print("Find Candidate SVs")
+    #
+    # ref_dict = reference.initial_fa(args.ref)
+    # cdel,cins,cinv,cdup=get_breakpoints(args.tumor,args.min_support_read,args.min_sv_len,args.max_sv_len,args.min_map_qual,ref_dict=ref_dict)
+    # # DEL:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[len_list],mean_left_confu,mean_right_confu]
+    # # INS:   [[pos,len,[read_name_list],insert_seq,[read_start_list],[read_end_list],[ref_start_list],[len_list],mean_left_confu,mean_right_confu]
+    # # INV:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[ref_end_list],mean_left_confu,mean_right_confu]
+    # # DUP:   [[pos,len,[read_name_list],[read_start_list],[read_end_list],[ref_start_list],[ref_end_list],mean_left_confu,mean_right_confu]
+    #
+    # with open(args.wkdir+'/tumor.candidate.bed','w') as fout:
+    #     for key in cdel:
+    #         chro=key
+    #         for bk in cdel[chro]:
+    #             fout.write(chro+'\t'+str(bk[0])+'\t'+str(bk[0]+bk[1])+'\t'+'DEL\t'+str(bk[1])+'\n')
+    #     for key in cins:
+    #         chro=key
+    #         for bk in cins[chro]:
+    #             fout.write(chro+'\t'+str(bk[0])+'\t'+str(bk[0]+1)+'\t'+'INS\t'+str(bk[1])+'\n')
+    #     for key in cinv:
+    #         chro=key
+    #         for bk in cinv[chro]:
+    #             fout.write(chro + '\t' + str(bk[0]) + '\t' + str(bk[0] + bk[1]) + '\t' + 'INV\t'+str(bk[1])+'\n')
+    #     for key in cdup:
+    #         chro=key
+    #         for bk in cdup[chro]:
+    #             fout.write(chro + '\t' + str(bk[0]) + '\t' + str(bk[0] + bk[1]) + '\t' + 'DUP\t'+str(bk[1])+'\n')
+    # print("Generate Features for Cancidate SVs")
+    # run(cdel,cins,cinv,cdup,ref_dict,args.tumor,args.normal,args.wkdir,int(args.t))
     print("Predict...")
     predict(args.model,args.wkdir,args.output)
     print(time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime()))
