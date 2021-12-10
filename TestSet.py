9d32f0824e49700ed4bfb734e615b9bcffadef13
mzduan <278326988@qq.com>
2021-12-10 23:24:32 +0800

    update

--- TestSet.py
+++ TestSet.py
@@ -8,6 +8,13 @@ class TestSet(data.Dataset):
         self.features=list()
         self.sup_features=list()
         self.file_names=list()
+
+        n_flag = False
+        t_flag = False
+        sup_flag = False
+
+
+
         for f in os.listdir(feat_path):
             splits=f.split('_')
             if len(splits)==5:
@@ -15,22 +22,34 @@ class TestSet(data.Dataset):
                 sub_feature = np.zeros((6, 50, 500))
                 for image in os.listdir(absolute_path):
                     if image[0] == 'n':
-                        normal = Image.open(absolute_path + '/' + image)
-                        normal = np.array(normal)
-                        sub_feature[3] = normal[:, :, 0]
-                        sub_feature[4] = normal[:, :, 1]
-                        sub_feature[5] = normal[:, :, 2]
+                        try:
+                            normal = Image.open(absolute_path + '/' + image)
+                            normal = np.array(normal)
+                            sub_feature[3] = normal[:, :, 0]
+                            sub_feature[4] = normal[:, :, 1]
+                            sub_feature[5] = normal[:, :, 2]
+                            n_flag = True
+                        except:
+                            print("Error in load " + absolute_path + '/' + image)
                     elif image[0] == 't':
-                        tumor = Image.open(absolute_path + '/' + image)
-                        tumor = np.array(tumor)
-                        sub_feature[0] = tumor[:, :, 0]
-                        sub_feature[1] = tumor[:, :, 1]
-                        sub_feature[2] = tumor[:, :, 2]
+                        try:
+                            tumor = Image.open(absolute_path + '/' + image)
+                            tumor = np.array(tumor)
+                            sub_feature[0] = tumor[:, :, 0]
+                            sub_feature[1] = tumor[:, :, 1]
+                            sub_feature[2] = tumor[:, :, 2]
+                        except:
+                            print("Error in load " + absolute_path + '/' + image)
                     elif image[0] == 's':
-                        sup_feat = np.load(absolute_path + '/' + image)
-                        self.sup_features.append(sup_feat)
-                self.features.append(sub_feature)
-                self.file_names.append(f)
+                        try:
+                            sup_feat = np.load(absolute_path + '/' + image)
+                            self.sup_features.append(sup_feat)
+                        except:
+                            print("Error in load " + absolute_path + '/' + image)
+
+                if n_flag and t_flag and sup_flag:
+                    self.features.append(sub_feature)
+                    self.file_names.append(f)
 
     def __len__(self):
         return len(self.features)
