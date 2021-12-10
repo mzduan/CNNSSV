6647de1308c2892c052e05797d5c485295a5da3c
mzduan <278326988@qq.com>
2021-12-10 11:29:41 +0800

    update

--- test/test.py
+++ test/test.py
@@ -1,4 +1,7 @@
 import pysam
 from PIL import Image
+import numpy as np
 if __name__ == '__main__':
-    normal=Image.open('/home/mzduan/somaticSV/CCS/tag_with_confusion/bam_0.7/chr22/germline/chr22_DUP_31943985_492_42/tumor.png')
+    # normal=Image.open('/home/mzduan/somaticSV/CCS/tag_with_confusion/bam_0.7/chr22/germline/chr22_DUP_31943985_492_42/tumor.png')
+    sup_feat = np.load('/home/mzduan/somaticSV/CCS/tag_with_confusion/bam_0.7/chr22/germline/chr22_DUP_36384861_509_50/sup_feat.npy')
+    print(sup_feat)
