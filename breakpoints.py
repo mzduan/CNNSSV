4d0ebea5603fbd6d752dec6e9966ba8911cdee48
mzduan <278326988@qq.com>
2022-01-03 15:17:19 +0800

    update

--- breakpoints.py
+++ breakpoints.py
@@ -1098,20 +1098,20 @@ def get_breakpoints(bam_file,min_support=1,min_sv_len=50,max_sv_len=10000,min_ma
             if aln.is_supplementary:   #对于supplementary，只分析alignment
                 aln_breakpoints=analysis_alignment(aln,min_sv_len,ref_dict)
                 breakpoints.extend(aln_breakpoints)
-            # else:   #对于primary，分析alignment和split
-            #
-            #     aln_breakpoints=analysis_alignment(aln,min_sv_len,ref_dict)
-            #     breakpoints.extend(aln_breakpoints)
-            #     if aln.has_tag("SA"):
-            #         supps=retrieve_supp(aln)
-            #
-            #         if aln.is_reverse:
-            #             query=get_reverse_comp(aln.query_sequence)
-            #         else:
-            #             query=aln.query_sequence
-            #         split_breakpoints=analysis_split_read(supps,aln.query_name,aln.query_length,query,min_sv_len,ref_dict)
-            #         if split_breakpoints:
-            #             breakpoints.extend(split_breakpoints)
+            else:   #对于primary，分析alignment和split
+
+                aln_breakpoints=analysis_alignment(aln,min_sv_len,ref_dict)
+                breakpoints.extend(aln_breakpoints)
+                if aln.has_tag("SA"):
+                    supps=retrieve_supp(aln)
+
+                    if aln.is_reverse:
+                        query=get_reverse_comp(aln.query_sequence)
+                    else:
+                        query=aln.query_sequence
+                    split_breakpoints=analysis_split_read(supps,aln.query_name,aln.query_length,query,min_sv_len,ref_dict)
+                    if split_breakpoints:
+                        breakpoints.extend(split_breakpoints)
 
 
     bam.close()
@@ -1123,8 +1123,7 @@ def get_breakpoints(bam_file,min_support=1,min_sv_len=50,max_sv_len=10000,min_ma
     #
     #
     #
-    # #将breakpoint排序再合并
-    # del_breakpoints=dict()
+    #将breakpoint排序再合并    # del_breakpoints=dict()
     # ins_breakpoints=dict()
     # dup_breakpoints=dict()
     # inv_breakpoints=dict()
@@ -1189,6 +1188,7 @@ def get_breakpoints(bam_file,min_support=1,min_sv_len=50,max_sv_len=10000,min_ma
     #     cdup[key]=cluster_DUP(dup_breakpoints[key],min_support,min_sv_len,max_sv_len)
     #     # [[pos,len,[read_name_list],...]
     # return cdel,cins,cinv,cdup,
+
 
 if __name__ == '__main__':
 
