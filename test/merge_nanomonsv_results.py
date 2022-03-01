7dd80f31e6da0814474c61fd9975d73773738d2c
mzduan <278326988@qq.com>
2022-03-01 17:49:29 +0800

    update

--- test/merge_nanomonsv_results.py
+++ test/merge_nanomonsv_results.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
 
     fout=open('/home/mzduan/somaticSV/COLO829_results/nanomonsv_results.vcf','w')
     for i in range(22,12,-1):
-        fin=open('/home/mzduan/somaticSV/COLO829_results/chr'+str(i)+'/tumor.nanomonsv.result.vcf','r')
+        fin=open('/home/mzduan/somaticSV/COLO829_results/chr'+str(i)+'/nanomonsv/tumor.nanomonsv.result.vcf','r')
 
         while True:
             l=fin.readline()
