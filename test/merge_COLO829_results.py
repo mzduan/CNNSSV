9f125b9c9fcf9b2b82867c738dff40eef64596fb
zhang570221322 <570221322@qq.com>
2022-03-12 15:46:43 +0800

    update

--- test/merge_COLO829_results.py
+++ test/merge_COLO829_results.py
@@ -1,13 +1,13 @@
 import re
 if __name__ == '__main__':
-    fout='/data/home/wlzhang/somaticSV/COLO829_results/nanomonsv/minimap2'
+    fout=open('/data/home/wlzhang/somaticSV/COLO829_results/cutesv/minimap2/COLO829_minimap2_cutesv.vcf','w')
     for t in range(1,23,1):
-        fin=open('/data/home/wlzhang/somaticSV/COLO829_results/nanomonsv/minimap2/chr'+str(t)+'/tumor.nanomonsv.result.vcf')
+        fin=open('/data/home/wlzhang/somaticSV/COLO829_results/cutesv/minimap2/cutesv_minimap2_somatic_chr'+str(t)+'.vcf')
         while True:
             l=fin.readline()
             if l:
                 infos=re.split('\s+',l)
-                if infos[0]=='#':
+                if infos[0][0]=='#':
                     continue
                 else:
                     fout.write(l)
