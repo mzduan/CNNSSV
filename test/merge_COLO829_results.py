53e7eba7f600f8055d908edd11d4ebddbf633a22
mzduan <278326988@qq.com>
2022-03-16 15:23:24 +0800

    update

--- test/merge_COLO829_results.py
+++ test/merge_COLO829_results.py
@@ -1,8 +1,8 @@
 import re
 if __name__ == '__main__':
-    fout=open('/data/home/wlzhang/somaticSV/COLO829_results/cutesv/ngmlr/COLO829_ngmlr_cutesv.vcf','w')
+    fout=open('/data/home/wlzhang/somaticSV/COLO829_results/sniffles/ngmlr/COLO829_ngmlr_sniffles.vcf','w')
     for t in range(1,23,1):
-        fin=open('/data/home/wlzhang/somaticSV/COLO829_results/cutesv/ngmlr/cutesv_ngmlr_somatic_chr'+str(t)+'.vcf')
+        fin=open('/data/home/wlzhang/somaticSV/COLO829_results/sniffles/ngmlr/sniffles_ngmlr_somatic_chr'+str(t)+'.vcf')
         while True:
             l=fin.readline()
             if l:
