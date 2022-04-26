eb85a0a9278589fc4ee219ce284c46553c539af0
mzduan <278326988@qq.com>
2022-04-26 13:45:07 +0800

    update

--- test/eliminate_random_seq.py
+++ test/eliminate_random_seq.py
@@ -2,8 +2,6 @@ import sys
 import re
 if __name__ == '__main__':
     sv_file_name = sys.argv[1]
-
-
     fout=open('filtered_'+sv_file_name,'w')
     with open(sv_file_name,'r') as fin:
         while True:
@@ -11,10 +9,10 @@ if __name__ == '__main__':
             if l:
                 infos=re.split('\s+',l)
                 if infos[3]=='tandem':
-                    infos[6]='0'
+                    fout.write(infos[0]+'\t'+infos[1]+'\t'+infos[2]+'\t'+"tandem duplication"+'\t'+infos[5]+'\t'+'0')
+
                 else:
-                    infos[5]='0'
-                fout.write(l)
+                    fout.write(infos[0]+'\t'+infos[1]+'\t'+infos[2]+'\t'+infos[3]+'\t'+infos[4]+'\t'+'0')
             else:
                 break
 
